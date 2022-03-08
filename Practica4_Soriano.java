e76f909f953012c7ed6d6c5c2a5f0c7e1514f083
Sharai Soriano <99279658+SharaiSoriano@users.noreply.github.com>
2022-03-07 20:39:08 -0800

    Add files via upload

---
+++ Practica4_Soriano.java
@@ -0,0 +1,50 @@
+import java.io.InputStream;
+
+class mexicano {
+
+    public mexicano(InputStream in) {
+    }
+
+public class main{
+
+    public static void mexicano(String[] args) {
+
+        mexicano m1 = new mexicano(System.in);
+        m1.nombre = "Maria";
+        m1.apepat = "Soriano";
+        m1.apemat = "Avila";
+        m1.genero = "Mujer";
+        m1.dianac = "14";
+        m1.mesnac = "10";
+        m1.yearnac = "01";
+        m1.estado = "Baja California";
+
+        System.out.println( "+m1.apepat.charAt(0)+");
+        System.out.println( "+m1.apepat.charAt(1)+");
+        System.out.println( "+m1.apemat.charAt(0)+");
+        System.out.println( "+m1.nombre.charAt(0)+");
+        System.out.println( "+m1.yearnac+");
+        System.out.println( "+m1.mesnac+");
+        System.out.println( "+m1.dianac+");
+        System.out.println( "+m1.genero.charAt(0)+");
+        System.out.println( "+m1.estado.charAt(0)+");
+        System.out.println( "+m1.estado.charAt(4)+");
+        System.out.println( "+m1.apepat.charAt(2)+");
+        System.out.println( "+m1.apemat.charAt(1)+");
+        System.out.println( "+m1.nombre.charAt(2)+");
+
+
+
+
+       
+
+
+       
+       
+       
+       
+        
+    }
+}
+
+    
